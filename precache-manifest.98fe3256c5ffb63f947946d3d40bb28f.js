f0e72ddf9e6e0d2ec744efaddff357b3debf1b86
Dan Gold <tentativechaos@gmail.com>
2019-11-09 14:59:20 -0800

    Updates

---
+++ precache-manifest.98fe3256c5ffb63f947946d3d40bb28f.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64bac3492be88432198bd26738f3a143",
+    "url": "/Dont-Match-the-Socks/index.html"
+  },
+  {
+    "revision": "ac900c89cc429b4f1175",
+    "url": "/Dont-Match-the-Socks/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "16f5033940d965140440",
+    "url": "/Dont-Match-the-Socks/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ac900c89cc429b4f1175",
+    "url": "/Dont-Match-the-Socks/static/js/2.b0429ca8.chunk.js"
+  },
+  {
+    "revision": "16f5033940d965140440",
+    "url": "/Dont-Match-the-Socks/static/js/main.4a5a9c0e.chunk.js"
+  },
+  {
+    "revision": "d93dad88f1edfb56412a",
+    "url": "/Dont-Match-the-Socks/static/js/runtime-main.4b13d99b.js"
+  },
+  {
+    "revision": "418c7bb6e17556cf2d5d27557e113d7b",
+    "url": "/Dont-Match-the-Socks/static/media/blackSock.418c7bb6.png"
+  },
+  {
+    "revision": "11699d2e82d7ee2737289d9db90c0490",
+    "url": "/Dont-Match-the-Socks/static/media/blueBlackArgyleSock.11699d2e.png"
+  },
+  {
+    "revision": "77be3dca17b97c2f89c8356e9d5c856d",
+    "url": "/Dont-Match-the-Socks/static/media/blueWhiteDotSock.77be3dca.png"
+  },
+  {
+    "revision": "afd152aed6744d7fe14443ac076b7f7f",
+    "url": "/Dont-Match-the-Socks/static/media/greenStripeSock.afd152ae.png"
+  },
+  {
+    "revision": "539f7e4c74f01dd7555979e558121c95",
+    "url": "/Dont-Match-the-Socks/static/media/greyBlueArgyleSock.539f7e4c.png"
+  },
+  {
+    "revision": "032f059bc7f7b3d3fec120767182e7ad",
+    "url": "/Dont-Match-the-Socks/static/media/greyPinkArgyleSock.032f059b.png"
+  },
+  {
+    "revision": "1ca13965c4f7eec9795fe0c831d65054",
+    "url": "/Dont-Match-the-Socks/static/media/greyPurpleArgyleSock.1ca13965.png"
+  },
+  {
+    "revision": "a59c8e1c1e1ddb1d7cd8e777a6975fe0",
+    "url": "/Dont-Match-the-Socks/static/media/greyPurpleDotSock.a59c8e1c.png"
+  },
+  {
+    "revision": "e74ba8866bb183ec0cea164c684e9ee3",
+    "url": "/Dont-Match-the-Socks/static/media/greyRedArgyleSock.e74ba886.png"
+  },
+  {
+    "revision": "0f31f62bf0a66cc4b64e2632cb1fe1c1",
+    "url": "/Dont-Match-the-Socks/static/media/greyRedDotSock.0f31f62b.png"
+  },
+  {
+    "revision": "87b697279af477fafe55dc0a6e7d0a15",
+    "url": "/Dont-Match-the-Socks/static/media/ojDotSock.87b69727.png"
+  },
+  {
+    "revision": "bfb1b60e19edbbf6e06bc3105c2cfdf6",
+    "url": "/Dont-Match-the-Socks/static/media/purpleStripeSock.bfb1b60e.png"
+  },
+  {
+    "revision": "dee828a2dec18af5f656b841a81ac9a0",
+    "url": "/Dont-Match-the-Socks/static/media/redDotSock.dee828a2.png"
+  },
+  {
+    "revision": "19015bf6b36b09ae3d8ed0846d9949f6",
+    "url": "/Dont-Match-the-Socks/static/media/yellowStripeSock.19015bf6.png"
+  }
+]);
